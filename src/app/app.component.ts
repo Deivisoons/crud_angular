903e0263961f54c9783d21c1546d93c84e70bda5
Marcio Santos <fisico227@gmail.com>
2020-01-27 11:38:23 -0300

    Enviando arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto02';
+
+  //Vetor
+  usuarios = [];
+
+  // Índice do usuário selecionado
+  indiceSelecionado = 0;
+  //Botões
+  btnCadastrar = true;
+  btnAlterar = false;
+  btnExcluir = false;
+  btnCancelar = false;
+
+  //Formulário
+  formulario = new FormGroup({
+    nome : new FormControl(''),
+    email : new FormControl(''),
+    idade : new FormControl('')
+  });
+
+  //Método de cadastro
+  cadastrar(){
+    this.usuarios.push(this.formulario.value);
+    this.formulario.reset();
+  }
+
+  //Método de selecionar
+  selecionar(indice){
+    this.formulario.setValue(this.usuarios[indice]);
+    this.btnCadastrar = false;
+    this.btnAlterar = true;
+    this.btnExcluir = true;
+    this.btnCancelar = true;
+  }
+
+  //Método de alterar
+  alterar(){
+    this.usuarios[this.indiceSelecionado] = this.formulario.value;
+    this.cancelar();
+  }
+
+  //Método de excluir
+  excluir(indice){
+    this.usuarios.splice(this.indiceSelecionado, 1);
+    this.cancelar();
+  }
+
+  //Método de cancelar
+  cancelar(){
+    this.formulario.reset();
+    this.btnCadastrar = true;
+    this.btnAlterar = false;
+    this.btnExcluir = false;
+    this.btnCancelar = false;
+  }
+
 }
